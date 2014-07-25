207009f22ef7eb4ba468756d4cb14c1d9a363a37
Chintan Choksi <chintan@Chintans-MacBook-Pro.local>
2014-07-25 14:29:26 -0400

    commit

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+setup(
+  name = 'overlord',
+  packages = [''], 
+  version = '0.1',
+  description = "Seattle's overlord package",
+  author = 'Seattle',
+  url = 'https://github.com/BuildSeash/overlord', # use the URL to the github repo
+  classifiers = [],
+)
