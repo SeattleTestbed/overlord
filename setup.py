17501e813e725f2452beffcaab1f87aeba1e3ed3
Chintan Choksi <chintan@Chintans-MacBook-Pro.local>
2014-07-25 22:44:19 -0400

    commit

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from distutils.core import setup
+from setuptools import setup
 setup(
   name = 'overlord',
   packages = [''], 
@@ -6,5 +6,6 @@ setup(
   description = "Seattle's overlord package",
   author = 'Seattle',
   url = 'https://github.com/BuildSeash/overlord', # use the URL to the github repo
+  dependency_links = 'https://github.com/BuildSeash/seash.git#egg=seash-1.0'
   classifiers = [],
 )
